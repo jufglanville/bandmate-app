afbd282927a34bc35a037f79d8bbb9ad032e6d80
Joe Glanville <josephglanville@gmail.com>
2025-01-23 17:12:34 +0000

    Add logs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
 import { handlers } from '@/lib/auth'; // Referring to the auth.ts we just created
+
+console.log('GITHUB_ID:', process.env.AUTH_GITHUB_ID);
+console.log('GITHUB_SECRET:', process.env.AUTH_GITHUB_SECRET);
+
 export const { GET, POST } = handlers;
